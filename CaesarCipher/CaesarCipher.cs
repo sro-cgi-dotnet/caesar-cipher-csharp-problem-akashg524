94fc63f63547a48373165f98d2d145ae1d04faa9
akashg524 <akashg524@gmail.com>
2018-09-14 16:02:02 +0530

    completed on 14/9/18

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,30 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string result="";
+            for (int i = 0; i < text.Length; i++)
+            {
+                int temp=Convert.ToInt32(text[i]);
+                if(Enumerable.Range(65,26).Contains(temp))
+                {
+                    temp=temp+shiftKey-65;
+                    temp=temp%26;
+                    temp+=65;
+                    result+=(char)temp;
+                }
+                else if(Enumerable.Range(97,26).Contains(temp))
+                {
+                    temp=temp+shiftKey-97;
+                    temp=temp%26;
+                    temp+=97;
+                    result+=(char)temp;
+                }
+                else
+                {
+                    result+=text[i];
+                }
+            }
+            return result;
         }
     }
 }
