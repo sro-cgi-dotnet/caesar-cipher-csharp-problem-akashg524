7def81a5f47f2f033ef88d8bd8fd2b7b2b713d56
akashg524 <akashg524@gmail.com>
2018-09-14 16:37:46 +0530

    Updated

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -11,14 +11,14 @@ namespace CaesarCipher
             for (int i = 0; i < text.Length; i++)
             {
                 int temp=text[i];
-                if(Enumerable.Range(65,26).Contains(temp))
+                if(Enumerable.Range(Convert.ToInt32('A'),26).Contains(temp))
                 {
                     temp=temp+shiftKey-65;
                     temp=temp%26;
                     temp+=65;
                     result+=(char)temp;
                 }
-                else if(Enumerable.Range(97,26).Contains(temp))
+                else if(Enumerable.Range(Convert.ToInt32('a'),26).Contains(temp))
                 {
                     temp=temp+shiftKey-97;
                     temp=temp%26;
